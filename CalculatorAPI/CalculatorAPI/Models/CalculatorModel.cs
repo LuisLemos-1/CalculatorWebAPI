5c74d2089718065a9ba8df4c31c601d07d2a5a58
Luis Lemos <81587263+LuisLemos-1@users.noreply.github.com>
2022-09-21 21:58:07 +0100

    Initial Setup
    Setup env
    Create model and controller for Calculator

---
+++ CalculatorAPI/CalculatorAPI/Models/CalculatorModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalculatorAPI.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        public int firstValue { get; set; }
+
+        [Required]
+        public int secondValue { get; set; }
+
+    }
+}
