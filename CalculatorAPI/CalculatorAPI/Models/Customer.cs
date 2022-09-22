8b74236c55ebeb4831df8a58f38825bf13e544ef
Ari Silva <ari.silva@hotmail.com>
2022-09-22 22:56:08 +0100

    Adding customer controler

---
+++ CalculatorAPI/CalculatorAPI/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculatorAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DOB { get; set; }
+    }
+}
