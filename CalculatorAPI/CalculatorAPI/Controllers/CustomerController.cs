5dab920fae5bd30c926fc274bd1e8c83a4561a01
Luis Lemos <81587263+LuisLemos-1@users.noreply.github.com>
2022-09-23 18:37:17 +0100

    CRUD methods for Customers

--- CalculatorAPI/CalculatorAPI/Controllers/CustomerController.cs
+++ CalculatorAPI/CalculatorAPI/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using CalculatorAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CalculatorAPI.Controllers
 {
@@ -40,6 +41,71 @@ namespace CalculatorAPI.Controllers
         public IActionResult GetAll()
         {
             return Ok(_customers);
+        }
+
+        // GET: api/Customer/id
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            Customer findCostumer = _customers.Find(e => e.Id == id);
+            return (findCostumer == null) ? NotFound() : Ok(findCostumer);
+
+            //return _customers.Find(e => e.Id == id) == null ? NotFound() : Ok(_customers.Find(e => e.Id == id));
+        }
+
+        // POST api/Customer
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            try
+            {
+                Customer customer = JsonConvert.DeserializeObject<Customer>(value);
+
+                // Tested with
+                // "{\"Id\":4,\"Name\":\"Ana Rodrigues\",\"DOB\":\"02/10/1990 00:00:00\"}"
+                _customers.Add(customer);
+                return Ok(customer);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        // PUT api/Customer/id
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            Customer findCostumer = _customers.Find(e => e.Id == id);
+            if (findCostumer == null)
+                return NotFound();
+
+            try
+            {
+                Customer updateCustomer = JsonConvert.DeserializeObject<Customer>(value);
+
+                // Tested with
+                // "{\"Id\":1,\"Name\":\"Jose Manuel\",\"DOB\":\"02/10/1990 00:00:00\"}"
+                _customers[_customers.FindIndex(e => e.Id == id)] = updateCustomer;
+                return Ok(updateCustomer);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+
+        }
+
+        // DELETE api/Customer/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Customer findCostumer = _customers.Find(e => e.Id == id);
+            if (findCostumer == null)
+                return NotFound();
+
+            _customers.Remove(findCostumer);
+            return Ok();
         }
     }
 }
