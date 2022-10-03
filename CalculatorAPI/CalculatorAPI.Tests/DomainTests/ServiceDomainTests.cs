1c6f3f9786e0d54523dd382d7b25626cdeb0b54c
Luis Lemos <81587263+LuisLemos-1@users.noreply.github.com>
2022-10-03 10:31:17 +0100

    Prepare Tests

---
+++ CalculatorAPI/CalculatorAPI.Tests/DomainTests/ServiceDomainTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAPI.Tests.DomainTests
+{
+    public class ServiceDomainTests
+    {
+        [Fact]
+        public void GetAll_GettingAllorNothing()
+        {
+            //Arrange
+            // Act
+            // Assert
+            throw new NotImplementedException();
+        }
+    }
+}
